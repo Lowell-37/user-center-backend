89ef70ed95983ead50cd3d155e3c5d05d36005fc
Lowell-37 <a2484822206@126.com>
2023-11-28 11:36:53 +0800

    add

---
+++ src/main/java/com/lowell/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.lowell.usercenter.mapper;
+
+import com.lowell.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
